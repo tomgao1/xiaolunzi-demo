43b559f7f263c35b81815a6c54f0ad09636d8b98
tomgao <406038564@qq.com>
2020-07-07 21:52:07 +0800

    稍微完成重构

--- src/col.vue
+++ src/col.vue
@@ -7,47 +7,54 @@
 </template>
 
 <script>
-      let validator = (value) => {
-          let keys = Object.keys(value)
-          let valid = true
-          keys.forEach(key => {
-              if(!['span','offset'].includes(key)) {
-                  valid = false
-              }
-          })
-          return valid
+ let validator = (value) => {
+    let keys = Object.keys(value)
+    let valid = true
+    keys.forEach(key => {
+      if (!['span', 'offset'].includes(key)) {
+        valid = false
       }
-    export default {
-        name: 'GuluCol',
-        props: {
-            span:{
-                type:[Number,String]
-            },
-            offset: {
-                type: [Number,String]
-            },
-            
-            ipad:{type:Object,validator},
-            narrowPc: {type:Object,validator,},
-            pc:{type:Object,validator,},
-            widePc: {type:Object,validator,},
-        },
+    })
+    return valid
+  }
+  export default {
+    name: 'GuluCol',
+    props: {
+      span: {
+        type: [Number, String]
+      },
+      offset: {
+        type: [Number, String]
+      },
+      ipad: {type: Object, validator,},
+      narrowPc: {type: Object, validator,},
+      pc: {type: Object, validator,},
+      widePc: {type: Object, validator,}
+    },
         data() {
             return {
                 gutter:0,
             }
         },
+        methods: {
+            createClasses (obj,str = '') {
+                if (!obj) {return []}
+                let array = []
+                if (obj.span) {array.push(`col-${str}${obj.span}`)}
+                if (obj.offset) { array.push(`offset-${str}${obj.offset}`)}
+                return array
+            }
+        },
          computed: {
       colClass () {
         let {span, offset,ipad,narrowPc,pc,widePc} = this
+        let createClasses = this.createClasses
         return [
-          span && `col-${span}`,
-          offset && `offset-${offset}`,
-          
-          ...(ipad ? [`col-ipad-${ipad.span}`]:[]),
-          ...(narrowPc ? [`col-narrow-pc-${narrowPc.sapn}`]:[]),
-          ... (pc ? [`col-pc-${pc.span}`]:[]),
-          ... (widePc ? [`col-wide-pc-${widePc.span}`]:[]),
+           ...createClasses({span,offset}),
+           ...createClasses(ipad, 'ipad-'),
+           ...createClasses(narrowPc, 'narrow-pc-'),
+           ...createClasses(pc, 'pc-'),
+           ...createClasses(widePc, 'wide-pc-'),
         ]
       },
       colStyle () {
@@ -62,22 +69,19 @@
 
 <style lang="scss" scoped>
   .col {
-      
-      $class-prefix: col-;
-      @for $n from 1 through 24 {
-          &.#{$class-prefix}#{$n} {
-              width: ($n / 24) *100%;
-          }
+    $class-prefix: col-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
       }
-      $class-prefix:offset-;
-      @for $n from 1 through 24 {
-           &.#{$class-prefix}#{$n} {
+    }
+    $class-prefix: offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
         margin-left: ($n / 24) * 100%;
-      
-           }
       }
-      
-      @media (min-width: 577px) {
+    }
+    @media (min-width: 577px) {
       $class-prefix: col-ipad-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
@@ -91,7 +95,7 @@
         }
       }
     }
-    @media (min-width: 769px) {
+    @media (min-width: 769px){ // 770
       $class-prefix: col-narrow-pc-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
@@ -105,7 +109,7 @@
         }
       }
     }
-    @media (min-width: 993px){
+    @media (min-width: 993px) {
       $class-prefix: col-pc-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
@@ -133,7 +137,6 @@
         }
       }
     }
-
   }
 
 </style>
