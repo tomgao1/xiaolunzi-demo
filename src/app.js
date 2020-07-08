4a24f742df27ea6d3ffc080542a58f3e44b7c4fd
tomgao <406038564@qq.com>
2020-07-08 16:21:13 +0800

    添加toast和plugin

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ import Header from './header'
 import Sider from './sider'
 import Content from './content'
 import Footer from './footer'
+import Toast from './toast'
+import plugin from './plugin'
 
 
 Vue.component('g-button',Button)
@@ -23,6 +25,8 @@ Vue.component('g-header',Header)
 Vue.component('g-sider',Sider)
 Vue.component('g-content',Content)
 Vue.component('g-footer',Footer)
+Vue.component('g-toast',Toast)
+Vue.use(plugin)
 
 
 new Vue ({
@@ -36,8 +40,8 @@ new Vue ({
     
     
     methods: {
-      inputChange (e) {
-        console.log(e)
+      showToast(){
+        this.$toast('我是message')
       }
     }
 }) 
