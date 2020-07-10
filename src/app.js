efec76af0ae7c07f184addd1bb0e04ca714ded96
tomgao <406038564@qq.com>
2020-07-10 12:58:07 +0800

    创建popover组件

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ import TabsHead from './tabs-head'
 import TabsBody from './tabs-body'
 import TabsItem from './tabs-item'
 import TabsPane from './tabs-pane'
+import Popover from './popover'
 
 
 
@@ -39,6 +40,7 @@ Vue.component('g-tabs-head',TabsHead)
 Vue.component('g-tabs-body', TabsBody)
 Vue.component('g-tabs-item', TabsItem)
 Vue.component('g-tabs-pane', TabsPane)
+Vue.component('g-popover', Popover)
 
 
 import createElement from 'vue'
