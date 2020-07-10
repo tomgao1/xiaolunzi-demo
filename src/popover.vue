dc4308397e33a305d1518240569179b5d27b6014
tomgao <406038564@qq.com>
2020-07-10 21:46:06 +0800

    添加默认样式

--- src/popover.vue
+++ src/popover.vue
@@ -4,7 +4,7 @@
         ref="contentWrapper">
             <slot name="content"></slot>
         </div>
-        <span ref="triggerWrapper">
+        <span ref="triggerWrapper" style="display:inline-block;">
             <slot></slot>
             </span>
     </div>
@@ -27,6 +27,8 @@
             if (this.$refs.popover && 
             (this.$refs.popover === e.target || this.$refs.popover.contains(e.target)))
             {return}
+            if(this.$refs.contentWrapper && 
+            (this.$refs.contentWrapper ===e.target || this.$refs.contentWrapper.contains(e.target))) {return}
             this.close()
         },
         open() {
@@ -44,6 +46,7 @@
               if(this.$refs.triggerWrapper.contains(event.target)) {
                   if (this.visible ===true) {
                       this.close()
+                      console.log('click close');
                   } else {
                       this.open()
                   }
@@ -55,6 +58,8 @@
 </script>
 
 <style lang="scss" scoped>
+ $border-color: #333;
+  $border-radius: 4px;
       .popover {
           display: inline-block;
           vertical-align: top;
@@ -63,8 +68,32 @@
           }
            .content-wrapper {
               position: absolute;
-              border: 1px solid red;
-              box-shadow: 0 0 3px rgba(0,0,0,0.5);
-              transform: translateY(-100%);
+              
+    border-radius: $border-radius;
+    filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
+    background: white;
+    transform: translateY(-100%);
+    margin-top: -10px;
+    padding: .5em 1em;
+    max-width: 20em;
+    word-break: break-all;
+    &::before, &::after {
+      content: '';
+      display: block;
+      border: 10px solid transparent;
+      width: 0;
+      height: 0;
+      position: absolute;
+      left: 10px;
+    }
+    &::before {
+      border-top-color: black;
+      top: 100%;
+    }
+    &::after {
+      border-top-color: white;
+      top: calc(100% - 1px);
+    }
+              
       }
 </style>
