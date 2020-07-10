01a7263103bcf48151d6b42a0673a26c07f79475
tomgao <406038564@qq.com>
2020-07-10 21:34:50 +0800

    重构tabs.vue

--- src/tabs.vue
+++ src/tabs.vue
@@ -32,23 +32,31 @@
         eventBus: this.eventBus
       }
     },
-    mounted () {
-     if (this.$children.length === 0) {
-        console && console.warn &&
-        console.warn('tabs的子组件应该是tabs-head和tabs-nav，但你没有写子组件')
-      }
-      this.$children.forEach((vm) => {
-        if (vm.$options.name === 'GuluTabsHead') {
-          vm.$children.forEach((childVm) => {
-            if (childVm.$options.name === 'GuluTabsItem'
-              && childVm.name === this.selected) {
-              this.eventBus.$emit('update:selected', this.selected, childVm)
-            }
-          })
-        }
-      })
+    method: {
+      checkChildren () {
+        if (this.$children.length === 0) {
+          console && console.warn &&
+          console.warn('tabs的子组件应该是tabs-head和tabs-nav，但你没有写子组件')
     }
-  }
+        },
+      selectTab () {
+        this.$children.forEach((vm) => {
+          if (vm.$options.name === 'GuluTabsHead') {
+            vm.$children.forEach((childVm) => {
+              if (childVm.$options.name === 'GuluTabsItem'
+                && childVm.name === this.selected) {
+                this.eventBus.$emit('update:selected', this.selected, childVm)
+              }
+            })
+          }
+        })
+    }
+  },
+   mounted () {
+      this.checkChildren();
+      this.selectTab();
+   }
+   }
 </script>
 
 <style lang="scss" scoped>
