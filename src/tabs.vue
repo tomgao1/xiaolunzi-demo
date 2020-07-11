aaba07a427cd44762507c750b81f2c83e98e65a8
tomgao <406038564@qq.com>
2020-07-11 14:27:56 +0800

    添加一个popover测试用例，另一个暂时没过，先跳

--- src/tabs.vue
+++ src/tabs.vue
@@ -32,13 +32,13 @@
         eventBus: this.eventBus
       }
     },
-    method: {
+    methods: {
       checkChildren () {
         if (this.$children.length === 0) {
           console && console.warn &&
           console.warn('tabs的子组件应该是tabs-head和tabs-nav，但你没有写子组件')
-    }
-        },
+        }
+      },
       selectTab () {
         this.$children.forEach((vm) => {
           if (vm.$options.name === 'GuluTabsHead') {
@@ -50,13 +50,13 @@
             })
           }
         })
-    }
-  },
-   mounted () {
+      }
+    },
+    mounted () {
       this.checkChildren();
       this.selectTab();
-   }
-   }
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
