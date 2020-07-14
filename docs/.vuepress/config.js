e663c7dd9b1199ce0f4515a0559a9c1dff067e51
tomgao <406038564@qq.com>
2020-07-14 15:56:30 +0800

    完成Button的展示

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,4 @@
+ base:'/xiaolunzi-demo',
 module.exports = {
     title: '轱辘UI',
     description: '一个好用的UI框架',
@@ -17,7 +18,17 @@ module.exports = {
         },
         {
           title: '组件',
-          children: ['/components/button']
+          children: [
+            
+              '/components/button',
+              '/components/tabs',
+              '/components/input',
+              '/components/grid',
+              '/components/layout',
+              '/components/toast',
+              '/components/popover',
+              '/components/collapse',
+          ]
         },
   
       ]
