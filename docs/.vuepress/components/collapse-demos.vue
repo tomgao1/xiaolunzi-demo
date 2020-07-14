79dc7a1c53ecdd2a667618406309be90f62a8f1b
tomgao <406038564@qq.com>
2020-07-14 19:06:19 +0800

    完成toast组件展示

---
+++ docs/.vuepress/components/collapse-demos.vue
@@ -0,0 +1,46 @@
+<template>
+
+ <div id="app" style="padding: 100px;">
+     <g-collapse :selected="selected">
+       <g-collapse-item title="标题1" name="1">内容1</g-collapse-item>
+       <g-collapse-item title="标题2" name="2">内容2</g-collapse-item>
+       <g-collapse-item title="标题3" name="3">内容3</g-collapse-item>
+     </g-collapse>
+     
+    <p>
+      <strong>代码</strong>
+    </p>
+    <pre><code>{{content}}</code></pre>
+    </div>
+</template>
+
+<script>
+ import Collapse from '../../../src/collapse'
+ import CollapseItem from '../../../src/collapse-item'
+    export default {
+        components: {
+            'g-collapse':Collapse,
+            'g-collapse-item':CollapseItem,
+        },
+          data () {
+              
+      return {
+          selected: [1],
+        content: `
+          selected:'1',
+          <g-collapse :selected="selected">
+       <g-collapse-item title="标题1" name="1">内容1</g-collapse-item>
+       <g-collapse-item title="标题2" name="2">内容2</g-collapse-item>
+       <g-collapse-item title="标题3" name="3">内容3</g-collapse-item>
+     </g-collapse>
+      `.replace(/^ {8}/gm, '').trim()
+      }
+    }
+  }
+        
+    
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- docs/components/collapse.md
+++ docs/components/collapse.md
@@ -2,3 +2,8 @@
 title: Collapse
 ---
 # 折叠面板
+
+<ClientOnly>
+<collapse-demos></collapse-demos>
+
+</ClientOnly>
